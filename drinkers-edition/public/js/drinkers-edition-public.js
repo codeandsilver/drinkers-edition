a2e2d0cc391d669ecf904117d6367858272e0474
codeandsilver <mj@codeandsilver.com>
2017-04-16 23:53:57 -0700

    JS For Heart'd Deals

--- drinkers-edition/public/js/drinkers-edition-public.js
+++ drinkers-edition/public/js/drinkers-edition-public.js
@@ -1,32 +1,35 @@
 (function( $ ) {
-	'use strict';
+	'use strict';	
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$(document).ready(function(){
+        $('.heart').click(function(){
+        	var id = $(this).attr("data-postid");        	
+            $.ajax({
+                type: 'POST',
+                data: "postid=" + id + "&userid=" + userid,
+                url: '/drinkers/wp-content/plugins/drinkers-edition/script.php',                
+                success: function(data) {                    
+                   $('#heart-'+id).css("color", "#d90000");
+                   $('#heart-'+id).css("background-color", "#fff");                   
+                }
+            });
+   	});
+   	
+   	$('.hearted').click(function(){
+        	var id = $(this).attr("data-postid");        	
+            $.ajax({
+                type: 'POST',
+                data: "postid=" + id + "&userid=" + userid,
+                url: '/drinkers/wp-content/plugins/drinkers-edition/delete.php',                
+                success: function(data) {                    
+                   $('#hearted-'+id).css("color", "#cb6d6d");
+                   $('#hearted-'+id).css("background-color", "#fff");                   
+                }
+            });
+   	});
+   	
+	});
+
+
 
 })( jQuery );
